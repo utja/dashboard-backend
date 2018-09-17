1370f4364428c1db194d222af0441fd52c9738c4
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-09-16 20:15:05 -0400

    add news api gem and request for top headlines

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,7 +1,12 @@
-class Api::V1::NewsController < ApplicationController
+require 'news-api'
 
+class Api::V1::NewsController < ApplicationController
+  
   def index
-    render json: [{id: 1, title: 'Hello World', body: 'Be a Programmer'}, {id: 2, title: 'Womp Womp', body: 'Wompity Womp Womp'}]
+    n = News.new(ENV["THEWORD"])
+    news = n.get_top_headlines(country: 'us', language: 'en')
+    # render json: [{id: 1, title: 'Hello World', body: 'Be a Programmer'}, {id: 2, title: 'Womp Womp', body: 'Wompity Womp Womp'}]
+    render json: news
   	# posts = Post.all
     # render json: {status: 'SUCCESS', message: 'Loaded all posts', data: posts}, status: :ok
   end
